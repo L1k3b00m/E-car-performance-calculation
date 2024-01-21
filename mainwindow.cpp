6b0ab8ca33d1ce92927ff410c6a586fec01de303
luo <674498248@qq.com>
2024-01-21 11:51:34 +0800

    初始

---
+++ mainwindow.cpp
@@ -0,0 +1,62 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{   //获取输入框数据
+    double power_input = ui->PowerEdit->text().toDouble();
+    double rpm_input = ui->RpmEdit->text().toDouble();
+    bool powerIsEmpt = ui->PowerEdit->text().isEmpty();
+    bool rpm_isEmpt = ui->RpmEdit->text().isEmpty();
+    //获取勾选框数据
+    ui->Result_text->setText("");//textEdit清空数据
+    //判断
+    if(powerIsEmpt&&rpm_isEmpt){
+        QMessageBox::critical(this,"出错了","没有输入数据！","重试");
+    }
+    if(!ui->Force_Check->isChecked()&&!ui->housePower_Check->isChecked()){
+        QMessageBox::critical(this,"出错了","没有勾选数据处理方式","重试");
+    }
+
+    if(ui->housePower_Check->isChecked()){
+        if(powerIsEmpt){
+                QMessageBox::warning(this,"警告","没有监测到数据输入","请重试");
+        }
+        else{
+            double power = HP_Cal(power_input);
+            ui->Result_text->append("马力为"+QString::number(power,'f',5)+"（Hp）");//QString为转换函数
+        }
+    }
+    //判断
+    if(ui->Force_Check->isChecked()){
+        if(!rpm_isEmpt && !powerIsEmpt){
+           double Force = Force_Cal(power_input,rpm_input);
+           ui->Result_text->append("扭矩为"+QString::number(Force,'f',5)+"（牛N/米M）");
+        }else if(rpm_isEmpt){
+            QMessageBox::warning(this,"警告","需要转速数据","请重试");
+        }else if(powerIsEmpt){
+            QMessageBox::warning(this,"警告","需要功率数据","请重试");
+        }
+    }
+
+}
+double MainWindow::HP_Cal(double power_input){
+    return power_input/0.735;
+}
+double MainWindow::Force_Cal(double power_input,double rpm_Input){
+    return (9550*power_input) / rpm_Input ;
+}
+//TODO马力倒推转速
+//TODO勾选框条件出现输入栏
